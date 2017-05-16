c70ee58737f0b7b7d218ba01d76b1db0a13a2328
Oussama Zaki <oussama.zaki@betssongroup.com>
2017-05-16 18:38:54 +0200

    removed unused import

--- api.cotato.io/src/judge/filewriter.ts
+++ api.cotato.io/src/judge/filewriter.ts
@@ -1,6 +1,5 @@
 import * as fs from "fs";
 import * as Promise from "bluebird";
-import { Observable } from "rxjs/Observable";
 
 export function writeFile(path, content): Promise<string> {
   return new Promise<string>((resolve, reject) => {
